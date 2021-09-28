fbab8dd8de7eecc403029585aded6b57ccac1327
Swarna Chikkala <91174586+swarnachikkala@users.noreply.github.com>
2021-09-28 00:45:00 -0400

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+/*
+a = 10;
+msg = "Square of " + a + " is " + power(a,2);
+Info msg;
+Info "Result: " + msg;
+*/
+
+/*
+qty = 10;
+price = 12.5;
+msg = "Total Bill Amount for " + qty + "(items) of each " + price + " is " + qty * price;
+Info msg;
+*/
+
+/*
+newsletter_optin = true;
+if (newsletter_optin)
+{
+	Info "Subscribed for Newsletters";
+}
+else
+{	Info "Unsubscribed from Newletters";
+}
+*/
+
+/*
+qty = 13.5;
+price = 12.5;
+amount = qty * price;
+Info amount;
+Info ceil(amount);
+Info floor(amount);
+Info round(amount,0);
+*/
+
