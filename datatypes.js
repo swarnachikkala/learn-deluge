039cc7c24d8e78b8018a439fa883a7cb00ae8cba
Swarna Chikkala <91174586+swarnachikkala@users.noreply.github.com>
2021-09-28 23:17:01 -0400

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -33,3 +33,43 @@ Info floor(amount);
 Info round(amount,0);
 */
 
+student1 = "145";
+student2 = "29";
+firstname = "Swarna";
+middlename = " ";
+lastname = "Chikkala";
+if( middlename.isEmpty())
+{
+fullname = firstname + " " + lastname;
+}
+else 
+{
+fullname = firstname + " " + middlename + " " + lastname;
+}
+addressline1 = "536 Catalina Grande Dr";
+hex_number = "41";
+
+
+Info fullname;
+Info fullname.contains("war");
+Info fullname.length();
+Info firstname.concat(" ").concat(lastname);
+Info fullname.startsWith("S");
+Info fullname.endsWithIgnoreCase("A");
+Info firstname.equalsIgnoreCase("swarna");
+Info addressline1.getAlpha();
+Info fullname.getOccurenceCount("a");
+Info fullname.getPrefix("w");
+Info fullname.getPrefix("Chikkala");
+
+Info hex_number.hexToText();
+Info fullname.indexOf("a")+1;
+Info fullname.lastIndexOf("a")+1;
+Info fullname.left(6);
+Info fullname.right(9);
+Info firstname.rightpad(10).concat(lastname);
+
+Info student1.leftPad(5);
+Info student2.leftpad(5);
+
+
